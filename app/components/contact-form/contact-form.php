d41b548ee0454fe4ff72eda3c1d926c528d2da61
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2021-11-20 02:49:59 +0200

    Contact Form styles

--- app/components/contact-form/contact-form.php
+++ app/components/contact-form/contact-form.php
@@ -9,15 +9,15 @@
     ?>
     <div class="contact-form__input-group">
         <label for="name">Your Name</label>
-        <input id="name" type="text" name="name" />
+        <input id="name" type="text" name="name" class="contact-form__input" />
     </div>
     <div class="contact-form__input-group">
         <label for="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" class="contact-form__input" />
     </div>
     <div class="contact-form__input-group">
         <label for="message">Message</label>
-        <textarea id="message" name="message"></textarea>
+        <textarea id="message" name="message" class="contact-form__textarea"></textarea>
     </div>
     <button>Send Message</button>
 </form>
